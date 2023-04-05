2906ec7a6a65606b28e7bfb3ae5bdd049a836475
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-05 17:27:15 -0400

    added editable plots!

---
+++ Plot.js
@@ -0,0 +1,10 @@
+class Plot{
+    constructor(text, id, sprite, x, y)
+    {
+        this.text = text,
+        this.id = id,
+        this.sprite = sprite,
+        this.x = x,
+        this.y = y
+    }
+}
--- main.js
+++ main.js
@@ -1,13 +1,116 @@
 console.log("Online");
 
 let app = new PIXI.Application({
-    width: window.visualViewport.width * .9,
+    width: 800,
     heigth: 450,
     backgroundColor: 0xdddddd
 }); // 16:9 * 50
 
 app.view.id = "stage";
 document.body.appendChild(app.view);
+
+window.addEventListener('load', () => {
+
+    let modalDialog = document.getElementsByClassName("modalDialog")[0];
+    modalDialog.className = "modalDialog-hidden";
+    let tempPlotID;
+
+    let plotList = []
+
+    document.querySelector('#modalButton-save').addEventListener('click', () =>
+    {
+        // console.log(modalDialog);
+        modalDialog.className  = "modalDialog-hidden";
+
+        if(tempPlotID != '')
+        {
+            let tPlot = findPlotByID(tempPlotID);
+
+            let modalD = document.querySelector('#modalText');
+            let modalTitle = document.querySelector('#modalTitle');
+            
+            tPlot.text = modalD.value;
+            
+            //console.log(tempPlotID);
+            console.log(tPlot.text);
+
+            modalD.value = '';
+            modalTitle.innerHTML = '';
+            tempPlotID = '';
+        }
+    });
+
+    document.querySelector('#modalButton-cancel').addEventListener('click', () =>
+    {
+        // console.log(modalDialog);
+        modalDialog.className  = "modalDialog-hidden";
+
+        if(tempPlotID != '')
+        {
+            let modalD = document.querySelector('#modalText');
+            let modalTitle = document.querySelector('#modalTitle');
+            
+            //console.log(tempPlotID);
+            let tPlot = findPlotByID(tempPlotID);
+            console.log(tPlot.text);
+
+            modalD.value = '';
+            modalTitle.innerHTML = '';
+            tempPlotID = '';
+        }
+    });
+
+    addPlot(); //TESTING
+    addPlot();
+    addPlot();
+
+    function addPlot()
+    {
+        let plotS = PIXI.Sprite.from('img/plot.png');
+        let tId = "plot"+plotList.length;
+
+        plotS.x = plotList.length * 150;
+
+        plotS.id = tId;
+        plotList.push(new Plot('', tId, plotS));
+
+        plotS.interactive = true;
+
+        app.stage.addChild(plotS);
+
+        plotS.addEventListener("click", (e) =>
+        {
+            modalDialog.className  = "modalDialog";
+            tempPlotID = e.target.id;
+    
+            let modalD = document.querySelector('#modalText');
+            let modalTitle = document.querySelector('#modalTitle');
+
+            let tPlot = findPlotByID(tempPlotID);
+
+            modalD.value = tPlot.text;
+            modalTitle.innerHTML = tPlot.id;
+
+            //console.log(e);
+        });
+    }
+
+    function findPlotByID(id)
+    {
+        for(let i = 0; i < plotList.length; i++)
+        {
+            if(plotList[i].id == id)
+            {
+                return plotList[i];
+            }
+        }
+
+        return false;
+    }
+
+    console.log(plotList);
+
+});
 
 // let scene1BG = PIXI.Sprite.from('img/bg-two.png');
 // scene1BG.width = app.view.width;
