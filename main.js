78b2f4ff8108dda965f2a0d7cac0147ade9e66da
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-05 11:24:44 -0400

    started on PIXI canvas

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 console.log("Online");
 
+let app = new PIXI.Application({
+    width: window.visualViewport.width * .9,
+    heigth: 450,
+    backgroundColor: 0xdddddd
+}); // 16:9 * 50
+
+app.view.id = "stage";
+document.body.appendChild(app.view);
+
+// let scene1BG = PIXI.Sprite.from('img/bg-two.png');
+// scene1BG.width = app.view.width;
+// scene1BG.height = app.view.height;
+// app.stage.addChild(scene1BG);
+
+// let fish1 = PIXI.Sprite.from("img/fish-1.png");
+// fish1.x = 200;
+// fish1.y = 100;
+// fish1.anchor.set(.5);
+// fish1.rotation = 45;
+// fish1.tint = 0xffaaaa;
+// fish1.width = 100;
+// fish1.scale.x = 2;
