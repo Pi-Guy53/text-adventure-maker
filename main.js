44bea7838f351ad4c78737ad1bda790947eeb9d2
UsrnGamesInc <71029215+Pi-Guy53@users.noreply.github.com>
2023-04-10 22:32:01 -0400

    fixed some styling, and added some more functionality

--- main.js
+++ main.js
@@ -1,116 +1,145 @@
 console.log("Online");
 
-let app = new PIXI.Application({
-    width: 800,
-    heigth: 450,
-    backgroundColor: 0xdddddd
-}); // 16:9 * 50
+let modalDialog = document.getElementsByClassName("modalDialog")[0];
+modalDialog.className = "modalDialog-hidden";
+let tempPlotID;
 
-app.view.id = "stage";
-document.body.appendChild(app.view);
+let plotList = []
 
-window.addEventListener('load', () => {
-
-    let modalDialog = document.getElementsByClassName("modalDialog")[0];
+document.querySelector('#modalButton-save').addEventListener('click', () => {
+    // console.log(modalDialog);
     modalDialog.className = "modalDialog-hidden";
-    let tempPlotID;
 
-    let plotList = []
+    if (tempPlotID != '') {
+        let tPlot = findPlotByID(tempPlotID);
 
-    document.querySelector('#modalButton-save').addEventListener('click', () =>
-    {
-        // console.log(modalDialog);
-        modalDialog.className  = "modalDialog-hidden";
+        let modalD = document.querySelector('#modalText');
+        let modalTitle = document.querySelector('#modalTitle');
 
-        if(tempPlotID != '')
-        {
-            let tPlot = findPlotByID(tempPlotID);
+        tPlot.text = modalD.value;
 
-            let modalD = document.querySelector('#modalText');
-            let modalTitle = document.querySelector('#modalTitle');
-            
-            tPlot.text = modalD.value;
-            
-            //console.log(tempPlotID);
-            console.log(tPlot.text);
+        //console.log(tempPlotID);
+        console.log(tPlot.text);
 
-            modalD.value = '';
-            modalTitle.innerHTML = '';
-            tempPlotID = '';
-        }
+        modalD.value = '';
+        modalTitle.innerHTML = '';
+        tempPlotID = '';
+    }
+});
+
+document.querySelector('#modalButton-cancel').addEventListener('click', () => {
+    // console.log(modalDialog);
+    modalDialog.className = "modalDialog-hidden";
+
+    if (tempPlotID != '') {
+        let modalD = document.querySelector('#modalText');
+        let modalTitle = document.querySelector('#modalTitle');
+
+        //console.log(tempPlotID);
+        let tPlot = findPlotByID(tempPlotID);
+        console.log(tPlot.text);
+
+        modalD.value = '';
+        modalTitle.innerHTML = '';
+        tempPlotID = '';
+    }
+});
+
+addPlot(); //TESTING
+addPlot();
+addPlot();
+
+function addPlot() {
+    let plotS = PIXI.Sprite.from('img/plot.png');
+    let tId = "plot-" + plotList.length;
+
+    plotS.anchor.set(.5, .5);
+    plotS.width = 100;
+    plotS.height = plotS.width * .75;
+
+    plotS.x = 60 + plotList.length * 110;
+    plotS.y = 75;
+
+    plotS.id = tId;
+    plotList.push(new Plot('', tId, plotS));
+
+    plotS.eventMode = 'static';
+    //plotS.interactive = true;
+
+    plotTitle = new PIXI.Text(tId);
+    plotTitle.anchor.set(.5, 1.5);
+    plotTitle.width = plotS.width * .5;
+
+    plotS.addChild(plotTitle);
+    app.stage.addChild(plotS);
+
+    plotS.addEventListener("mouseup", (e) => {
+        modalDialog.className = "modalDialog";
+        tempPlotID = e.target.id;
+
+        let modalD = document.querySelector('#modalText');
+        let modalTitle = document.querySelector('#modalTitle');
+
+        let tPlot = findPlotByID(tempPlotID);
+
+        modalD.value = tPlot.text;
+        modalTitle.innerHTML = tPlot.id;
+
+        //console.log(e);
     });
 
-    document.querySelector('#modalButton-cancel').addEventListener('click', () =>
-    {
-        // console.log(modalDialog);
-        modalDialog.className  = "modalDialog-hidden";
+    plotS.addEventListener("rightdown", (e) => {
+        e.preventDefault();
+        console.log(e);
 
-        if(tempPlotID != '')
-        {
-            let modalD = document.querySelector('#modalText');
-            let modalTitle = document.querySelector('#modalTitle');
-            
-            //console.log(tempPlotID);
-            let tPlot = findPlotByID(tempPlotID);
-            console.log(tPlot.text);
-
-            modalD.value = '';
-            modalTitle.innerHTML = '';
-            tempPlotID = '';
-        }
+        //input custom menu
     });
+}
 
-    addPlot(); //TESTING
-    addPlot();
-    addPlot();
+//Prevent right clicks on the page for a custom context meun
+//would prefer to just add to the existing one, but not sure how to do that RN
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
 
-    function addPlot()
-    {
-        let plotS = PIXI.Sprite.from('img/plot.png');
-        let tId = "plot"+plotList.length;
+    //input custom menu
+});
 
-        plotS.x = plotList.length * 150;
-
-        plotS.id = tId;
-        plotList.push(new Plot('', tId, plotS));
-
-        plotS.interactive = true;
-
-        app.stage.addChild(plotS);
-
-        plotS.addEventListener("click", (e) =>
-        {
-            modalDialog.className  = "modalDialog";
-            tempPlotID = e.target.id;
-    
-            let modalD = document.querySelector('#modalText');
-            let modalTitle = document.querySelector('#modalTitle');
-
-            let tPlot = findPlotByID(tempPlotID);
-
-            modalD.value = tPlot.text;
-            modalTitle.innerHTML = tPlot.id;
-
-            //console.log(e);
-        });
-    }
-
-    function findPlotByID(id)
-    {
-        for(let i = 0; i < plotList.length; i++)
-        {
-            if(plotList[i].id == id)
-            {
-                return plotList[i];
-            }
-        }
-
-        return false;
-    }
+function deletePlot(id) {
 
     console.log(plotList);
 
-});
+    const removedPlot = plotList.find(item => item.id == id);
+    plotList = plotList.filter(item => item.id !== id);
+
+    app.stage.removeChild(removedPlot.sprite);
+
+    console.log(plotList);
+
+    renderPlots();
+}
+
+function findPlotByID(id) {
+    for (let i = 0; i < plotList.length; i++) {
+        if (plotList[i].id == id) {
+            return plotList[i];
+        }
+    }
+
+    return false;
+}
+
+function renderPlots() {
+    for (let i = 0; i < plotList.length; i++) {
+        plotList[i].sprite.x = 60 + (i * 110);
+        plotList[i].sprite.y = 75;
+    }
+}
+
+console.log(plotList);
+
+//!Window.localStorage
+//Store data on the local browser
+
 
 // let scene1BG = PIXI.Sprite.from('img/bg-two.png');
 // scene1BG.width = app.view.width;
